23a5812ec7613d56f594c4bc57b6617f1ad4543b
Andrew Shearer <andrewshearerdev@gmail.com>
2022-02-17 14:02:09 -0800

    updated blog-post to use functional syntax and removed a few packages

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,12 +9,14 @@ const Navigation = () => (
 			<span className={styles.logo} />
 			<span className={styles.navigationItem}>Gatsby Starter Contentful</span>
 		</Link>
+
 		<ul className={styles.navigation}>
 			<li className={styles.navigationItem}>
 				<Link to='/' activeClassName='active'>
 					Home
 				</Link>
 			</li>
+
 			<li className={styles.navigationItem}>
 				<Link to='/blog/' activeClassName='active'>
 					Blog
